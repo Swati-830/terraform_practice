e2c98dc6f4a4b66f872beb9ad530d0046d667a69
Swati <swatijaiswal90090@gmail.com>
2024-12-19 11:30:16 +0530

    --- Auto Git Commit ---

--- ec2.tf
+++ ec2.tf
@@ -1,3 +1,6 @@
+
+
+
 provider "aws" {
     region = "ca-central-1"
   
@@ -8,4 +11,11 @@ resource "aws_instance" "first" {
     instance_type = "t2.micro"
     key_name = "central"
     security_groups = ["default"]
+}
+
+variable "img_id" {
+    type = string
+    default = "ami-0bee12a638c7a8942"
+    description = "value of the image id"
+  
 }
