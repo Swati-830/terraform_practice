c255384833be36b09b41b0b77b3456f241970803
Swati <swatijaiswal90090@gmail.com>
2024-12-20 15:24:17 +0530

    --- Auto Git Commit ---

--- ec2.tf
+++ ec2.tf
@@ -1,47 +1,76 @@
 #  alias gp="git pull origin main"
 
 provider "aws" {
-    region = "ca-central-1"
+    region = "us-east-1"
 }
 
-resource "aws_instance" "first" {
-    ami = var.img_id
+data "aws_subnet" "my_subnet" {
+    id = var.subnet_id
+}
+
+resource "aws_security_group" "my_sg" {
+    name = "my-sg"
+    description = "allow HTTP Port"
+    ingress {
+        from_port        = 80
+        to_port          = 80
+        protocol         = "TCP"
+        cidr_blocks      = ["0.0.0.0/0"]
+    }
+    egress {
+        from_port        = 0
+        to_port          = 0
+        protocol         = "-1"
+        cidr_blocks      = ["0.0.0.0/0"]
+    }
+}
+
+resource "aws_instance" "instance_1" {
+    ami = var.image_id
     instance_type = var.instance_type
-    key_name = var.key_name
-    security_groups = var.sec_groups
-    vpc_security_group_ids = var.vpc_security_group_ids
+    key_name = var.key_pair
+#    security_groups = var.sg_name
+    vpc_security_group_ids = [aws_security_group.my_sg.id]
+    subnet_id = var.subnet_id
+    user_data = <<-EOF
+        #!/bin/bash
+        yum install httpd -y
+        systemctl start httpd
+        systemctl enable httpd
+    EOF
 }
 
-variable "img_id" {
-    type = string
-    default = "ami-0bee12a638c7a8942"
-    description = "value of the image id"
-  
+variable "image_id" {
+  type        = string
+  default     = "ami-012967cc5a8c9f891"
+  description = "Enter AMI ID"
 }
 
 variable "instance_type" {
-    type = string
     default = "t2.micro"
-    description = "value of the instance type"
-  
 }
 
-variable "key_name" {
-    type = string
-    default = "central"
-    description = "value of the key name"
-  
+variable "key_pair" {
+    default = "new-key-nv"
 }
 
-variable "sec_groups" {
-    type = list(string)
-    default = ["default"]
-    description = "value of the security groups"
-  
+variable "subnet_id" {
+    default = "subnet-091678d44aa5db41d"
 }
-variable "vpc_security_group_ids" {
-    type = list(string)
-    default = ["vpc-0086232a42236ffd5"]
-    description = "value of the security groups"
-  
+
+# variable "sg_name" {
+#     type = list
+#     default = ["default"]
+# }
+
+output "demo" {
+    value = "Hello World"
+}
+
+output "public_ip" {
+    value = aws_instance.instance_1.public_ip
+}
+
+output "arn_subnet" {
+    value = data.aws_subnet.my_subnet.arn
 }
